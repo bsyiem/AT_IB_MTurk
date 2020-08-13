5c0fa7c63169a0c76ed01f3b2483ded536f4c002
Shavedmullet <bsyiem92@gmail.com>
2020-08-13 12:42:39 +1000

    updated php scripts

---
+++ PHP/AR_AT/scripts/Pilot/setParticipantID.php
@@ -0,0 +1,16 @@
+<?php
+	header("Access-Control-Allow-Origin: *");
+	$pId = $_POST["pId"];
+
+	if($pId !== ""){
+		if(($file = fopen("../../files/Pilot/last_pid.csv","w")) !== FALSE){
+			if(flock($file,LOCK_EX)){
+				fputcsv($file,array($pId));
+			}else{
+				echo "Error: Please Reload Page";
+			}
+			fclose($file);
+		}
+	}
+	
+?>
