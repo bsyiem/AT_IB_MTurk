0910a68d714fd50ea97b9729aef9a7611bf6d90d
Shavedmullet <bsyiem92@gmail.com>
2020-08-18 18:55:16 +1000

    added starting scene

---
+++ Assets/BeginButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BeginButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BeginStudy()
+    {
+        SceneManager.LoadScene("MTurk Instructions");
+    }
+}
